bc2af48619507b35887af5764564180cb0be22cb
dawling <valenzueladfausto@gmail.com>
2020-02-05 17:03:42 -0700

    parte php

---
+++ ejemplo2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ejercicio2</title>
+</head>
+<body>
+    parte html
+    <br><br>
+    <?php
+    echo "Parte PHP <br>";
+    for($i=0;$i<10;$i++)
+    {
+        echo "Linea" .$i. "<br>";
+    }
+    ?>
+</body>
+</html>
